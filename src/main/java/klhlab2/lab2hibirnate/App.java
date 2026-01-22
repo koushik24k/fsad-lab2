23fd39ac23970fc13760f0429d90d0a8423e1beb
koushik <2410030319@klh.edu.in>
2026-01-22 19:56:18 +0530

    lab2

--- src/main/java/klhlab2/lab2hibirnate/App.java
+++ src/main/java/klhlab2/lab2hibirnate/App.java
@@ -5,25 +5,31 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+
 public class App 
 {
     public static void main( String[] args )
     {
-        Student s=new Student();
+    	
+        product pr = new product();
+        
         SessionFactory factory = new Configuration()
                 .configure()
-                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(product.class)
                 .buildSessionFactory();
-
+        
         Session session = factory.openSession();
         Transaction tx = session.beginTransaction();
         
-        s.setName("Ram");
-
-        session.persist(s);
+        pr.setName("Car");
+        pr.setDescription("Blue Car");
+        pr.setPrice(570000.25);
+        pr.setQuantity(1);
+        session.persist(pr);
         tx.commit();
-
+        
         session.close();
         factory.close();
     }
 }
+ 
---
+++ src/main/java/klhlab2/lab2hibirnate/product.java
@@ -0,0 +1,58 @@
+package klhlab2.lab2hibirnate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name= "Product1")
+public class product 
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	String name;
+	String description;
+	double price;
+	int quantity;
+	
+	public product()
+	{
+		
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	public String getDescription() 
+	{
+		return description;
+	}
+	public void setDescription(String description) 
+	{
+		this.description = description;
+	}
+	public double getPrice() 
+	{
+		return price;
+	}
+	public void setPrice(double price) 
+	{
+		this.price = price;
+	}
+	public int getQuantity() 
+	{
+		return quantity;
+	}
+	public void setQuantity(int quantity) 
+	{
+		this.quantity = quantity;
+	}	
+}
